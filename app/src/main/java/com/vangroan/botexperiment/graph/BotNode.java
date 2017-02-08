976058bce11190f2329721ec0427b8e8395677bb
Willem Victor <wimpievictor@gmail.com>
2017-02-08 20:48:44 +0200

    Added bot node

---
+++ app/src/main/java/com/vangroan/botexperiment/graph/BotNode.java
@@ -0,0 +1,8 @@
+package com.vangroan.botexperiment.graph;
+
+/**
+ * Created by Victor on 2017/02/08.
+ */
+
+public class BotNode {
+}
--- app/src/main/java/com/vangroan/botexperiment/view/adapter/BotHistoryAdapter.java
+++ app/src/main/java/com/vangroan/botexperiment/view/adapter/BotHistoryAdapter.java
@@ -3,7 +3,11 @@ package com.vangroan.botexperiment.view.adapter;
 import android.support.v7.widget.RecyclerView;
 import android.view.ViewGroup;
 
+import com.vangroan.botexperiment.graph.BotNode;
 import com.vangroan.botexperiment.view.viewholder.BotVH;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Victor on 2017/02/08.
@@ -11,7 +15,7 @@ import com.vangroan.botexperiment.view.viewholder.BotVH;
 
 public class BotHistoryAdapter extends RecyclerView.Adapter<BotVH> {
 
-//    private List<BotVH> history = new ArrayList<>();
+    private List<BotNode> history = new ArrayList<>();
 
     public BotHistoryAdapter() {
 
@@ -24,11 +28,11 @@ public class BotHistoryAdapter extends RecyclerView.Adapter<BotVH> {
 
     @Override
     public void onBindViewHolder(BotVH holder, int position) {
-
+        holder.populate();
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return history.size();
     }
 }
